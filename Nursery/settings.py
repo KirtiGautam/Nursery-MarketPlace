ec724e963f592ff0f8c55942dd8cbf5e3cba7511
KirtiGautam <gautamkirti8c@gmail.com>
2020-12-31 16:45:51 +0530

    Serve static files

--- Nursery/settings.py
+++ Nursery/settings.py
@@ -33,6 +33,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'Nursery.urls'
